e6f6e616a564ef40f5e55a3a6519060af297e67b
ChenBingYuan <smallg1009@gmail.com>
2021-08-09 19:23:25 +0800

    Initial Commit

---
+++ CCWorkTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CCWorkTest
+//
+//  Created by mac on 2021/8/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
